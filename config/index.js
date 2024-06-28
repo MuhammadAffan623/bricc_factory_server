06e2e38b8e0ab6b9bd52bd02c36166f49441173f
MuhammadAffan623 <affanmuhammad623@gmail.com>
2024-06-29 01:08:28 +0500

    feat: configured aws client using aws-sdk

--- config/index.js
+++ config/index.js
@@ -1,5 +1,11 @@
 const path = require("path");
+const AWS = require('aws-sdk');
 require("dotenv").config({ path: path.resolve(__dirname, "..", ".env") });
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY
+});
 
 module.exports = {
   MONGO_URL: process.env.MONGO_URL,
@@ -7,4 +13,7 @@ module.exports = {
   PORT: process.env.PORT,
   INFURA_PROJECT_ID: process.env.INFURA_PROJECT_ID,
   ERC20_CONTRACT_ADDRESS: process.env.ERC20_CONTRACT_ADDRESS,
+  s3,
+  S3_BUCKET_NAME : process.env.S3_BUCKET_NAME
+
 };
